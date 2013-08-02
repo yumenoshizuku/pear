548e5cc0a301aedfc863f2114ed2bf5dee79803c
Risto <risto1@gmail.com>
2013-08-02 15:23:52 -0400

    Change in plans         - The C pretty-printer fails under normal circumstances:         int main(){             printf("hello world.\n");             int a;             int *ap;             a = 5;             ap = &a;             return 0;         }         - New plan: Elaborate the MicroC interpreter into a             pretty printer.         - Phase 1: MicroC interpreter added         - Phase 2: Pretty print strings (the -a switch)         - Phase 3: Added window classes

---
+++ compiler/microc/microc.ml
@@ -0,0 +1,14 @@
+type action = Ast | Interpret
+
+let _ =
+  let action = if Array.length Sys.argv > 1 then
+    List.assoc Sys.argv.(1) [ ("-a", Ast);
+			      ("-i", Interpret)]
+  else Interpret in 
+  let lexbuf = Lexing.from_channel stdin in
+  let program = Parser.program Scanner.token lexbuf in
+  match action with
+    Ast -> let listing = Ast.string_of_program program
+           in print_string listing
+  | Interpret -> ignore (Interpret.run program)
+;;
